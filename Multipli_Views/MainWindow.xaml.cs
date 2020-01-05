e3ad78312cc4e1e07c63e0300ff91f43977061d5
DKMT <xrx1000_001@163.com>
2020-01-06 00:04:55 +0800

    2020001060004

---
+++ Multipli_Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Multipli_Views.ViewModels;
+
+namespace Multipli_Views
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedView_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new RedViewModel();
+        }
+
+        private void BlueView_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new BlueViewModel();
+        }
+    }
+}
