142f0259cf0401e7ae4ec50b8abe536105fd6797
mtdk <ruixiangxie@outlook.com>
2021-04-28 00:45:30 +0800

    202104281245

--- mumu_hellocsharp.cs
+++ mumu_hellocsharp.cs
@@ -1,9 +1,9 @@
 using System.Windows;
-
 class mumu_hellocsharp
 {
 	static void Main()
-		{
-			MessageBox.Show("Hello mumu!");
-		}
+	{
+		//Console.WriteLine("Hello mumu!");
+		MessageBox.Show("Hello mumu!");
+	}
 }
--- mumu_hellowpf.cs
+++ mumu_hellowpf.cs
@@ -1,17 +1,14 @@
-using System;
 using System.Windows;
-
 class mumu_hellowpf
 {
 	[STAThread]
 	public static void Main()
 	{
 		Window win = new Window();
-		win.Title = "Hello mumu";
+		win.Title = "Hello mumu!";
 		win.Width = 300;
 		win.Height = 200;
 		win.Show();
-
 		Application app = new Application();
 		app.Run();
 	}
